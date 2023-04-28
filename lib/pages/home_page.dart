071f149a2f66c4a4f444b712eb666ed4be153b5e
Ibrahim Khan <aitvetibrahim@gmail.com>
2023-04-28 19:53:05 +0600

    home page alert dialog done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,8 +8,56 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //text controllers
+  final newExpenseNameController = TextEditingController();
+  final newExpenseAmountController = TextEditingController();
+
+  //add new expense
+  void addNewExpense() {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: Text('Add new expense'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  //expense name
+                  TextField(
+                    controller: newExpenseNameController,
+                  ),
+
+                  //expense amount
+                  TextField(
+                    controller: newExpenseAmountController,
+                  )
+                ],
+              ),
+              actions: [
+                MaterialButton(
+                  onPressed: save,
+                  child: Text('SAVE'),
+                ),
+                MaterialButton(
+                  onPressed: cancel,
+                  child: Text('CANCEL'),
+                )
+              ],
+            ));
+  }
+
+  //save
+  void save(){}
+
+  //cancel
+  void cancel(){}
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      floatingActionButton: FloatingActionButton(
+        onPressed: addNewExpense,
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
