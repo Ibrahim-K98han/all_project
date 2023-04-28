81d8a96ef2686cb1cb718107f8bd0486a96fc4a6
Ibrahim Khan <aitvetibrahim@gmail.com>
2023-04-28 11:46:43 +0600

    addressbook_two project added

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,24 @@
-import 'package:address_book_practice/pages/contact_details_page.dart';
-import 'package:address_book_practice/pages/contact_list_page.dart';
-import 'package:address_book_practice/pages/new_contact_page.dart';
-import 'package:address_book_practice/providers/contact_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MultiProvider(providers: [
-    ChangeNotifierProvider(create: (context) => ContactProvider()..getAllContacts())
-  ], child: const MyApp()));
+  runApp(ContactApps());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ContactApps extends StatefulWidget {
+  const ContactApps({Key? key}) : super(key: key);
 
+  @override
+  State<ContactApps> createState() => _ContactAppsState();
+}
+
+class _ContactAppsState extends State<ContactApps> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blue
       ),
-      initialRoute: ContactListPage.routeName,
-      routes: {
-        ContactListPage.routeName: (context) => const ContactListPage(),
-        NewContactPage.routeName: (context) => const NewContactPage(),
-        ContactDetailsPage.routeName: (context) => const ContactDetailsPage(),
-      },
     );
   }
 }
